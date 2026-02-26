f22a7b4adc379ce3d809a7cdde8c0d279d45a88b
Baegus <baegus@gmail.com>
2026-02-26 17:44:50 +0100

    Fix upside-down image texture

--- src/js/WebGLitter.js
+++ src/js/WebGLitter.js
@@ -128,6 +128,7 @@ export default class WebGLitter {
 		img.onload = () => {
 			if (!this.particleTexture) this.particleTexture = gl.createTexture();
 			gl.bindTexture(gl.TEXTURE_2D, this.particleTexture);
+			gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, true);
 			gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, img);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR);
 			gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR);
